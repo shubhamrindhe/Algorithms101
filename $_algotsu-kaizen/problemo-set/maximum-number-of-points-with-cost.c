d051d9b53388c37f4a59e1724bb412d86d6235e5
Shubham Rindhe <rindheshubham@gmail.com>
2024-08-18 00:19:15 +0530

    2024.AUGUST.17

--- $_algotsu-kaizen/problemo-set/maximum-number-of-points-with-cost.c
+++ $_algotsu-kaizen/problemo-set/maximum-number-of-points-with-cost.c
@@ -31,6 +31,42 @@ int **box_2_ptr_d2(int rows, int columns, int arr[rows][columns])
     return array;
 }
 
+long long findMax(long long array[], int size) {
+    long long max = array[0];
+    for (int i = 1; i < size; i++)
+        if (array[i] > max)
+            max = array[i];
+    return max;
+}
+
+long long maxPoints(int **points, int pointsSize, int *pointsColSize)
+{
+    int R = pointsSize, C = *pointsColSize;
+    long long *dp = (long long *)malloc(C * sizeof(long long));
+
+    for (int i = 0; i < C; ++i) *(dp + i) = *(*(points) + i);
+
+    for (int row = 1; row < R; ++row)
+    {
+        long long *l2r = (long long *)malloc(C * sizeof(long long));
+        l2r[0] = *dp;
+        long long *r2l = (long long *)malloc(C * sizeof(long long));
+        r2l[C - 1] = *(dp + C - 1);
+
+        for (int l = 1; l < C; ++l) l2r[l] = MAX(l2r[l - 1] - 1, dp[l]);
+        for (int r = C - 2; r >= 0; --r) r2l[r] = MAX(r2l[r + 1] - 1, dp[r]);
+        for (int c = 0; c < C; ++c) dp[c] = points[row][c] + MAX(l2r[c], r2l[c]);
+
+        free(l2r);
+        free(r2l);
+    }
+
+    long long max = findMax(dp, C);
+    free(dp);
+
+    return max;
+}
+
 int main()
 {
 
@@ -46,7 +82,7 @@ int main()
         {3, 1, 1},
     };
     int in1_ = 3;
-    // printf("OUT [%d] == 9\n", maxPoints((int **)box_2_ptr_d2(3, 3, in1), 3, &in1_));
+    printf("OUT [%d] == 9\n", maxPoints((int **)box_2_ptr_d2(3, 3, in1), 3, &in1_));
 
     // [
     // [1, 5],
