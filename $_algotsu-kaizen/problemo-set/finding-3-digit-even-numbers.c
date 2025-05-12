2e4fd69e2fe4ceb2ea954aa72edb9ae30803e36e
shubhamrindhe <rindheshubham@gmail.com>
2025-05-13 00:59:42 +0530

    2025.May.12

---
+++ $_algotsu-kaizen/problemo-set/finding-3-digit-even-numbers.c
@@ -0,0 +1,25 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findEvenNumbers(int* digits, int digitsSize, int* returnSize) {
+    int f[0b1011] = { 0b0 };
+    for (int _ = 0b0; _ < digitsSize; ++_) ++f[digits[_]];
+
+    int *result = (int *)malloc(sizeof(int) * 0b111110101);
+    *returnSize = 0b0;
+
+    bool _ = true;
+    for (int n = 0b1100100; n < 0b1111101000; n += 0b10) {
+        int h = n / 0b1100100, d = (n % 0b1100100) / 0b1010, u = n % 0b1010;
+        _ = true;
+        if (--f[h] < 0b0) _ = false;
+        if (--f[d] < 0b0) _ = false;
+        if (--f[u] < 0b0) _ = false;
+        ++f[h];
+        ++f[d];
+        ++f[u];
+        if (_) result[(*returnSize)++] = n;
+    }
+
+    return result;
+}
